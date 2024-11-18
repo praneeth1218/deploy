c3001320906d0b00ad8ec894bc3dadc724a4c09b
praneeth1218 <praneetharceus@gmail.com>
2024-11-18 13:28:38 +0530

    Update VideoMeet.jsx

--- frontend/src/pages/VideoMeet.jsx
+++ frontend/src/pages/VideoMeet.jsx
@@ -525,22 +525,52 @@ export default function VideoMeetComponent() {
 
                     <video className={styles.meetUserVideo} ref={localVideoref} autoPlay muted></video>
 
-                    <div className={`${styles.conferenceView} ${videos.some(v => v.isScreenShare) ? styles.hasScreenShare : ''}`}>
-    {videos.map((video) => (
-        <div key={video.socketId}>
+                   <div className={`${styles.conferenceView} ${videos.some(v => v.isScreenShare) ? styles.hasScreenShare : ''}`}>
+    {videos.some(v => v.isScreenShare) ? (
+        <>
+            <div className="screenShareContainer">
+                {videos.filter(v => v.isScreenShare).map((video) => (
+                    <video
+                        key={video.socketId}
+                        data-socket={video.socketId}
+                        ref={ref => {
+                            if (ref && video.stream) {
+                                ref.srcObject = video.stream;
+                            }
+                        }}
+                        autoPlay
+                    />
+                ))}
+            </div>
+            <div className="participantsContainer">
+                {videos.filter(v => !v.isScreenShare).map((video) => (
+                    <video
+                        key={video.socketId}
+                        data-socket={video.socketId}
+                        ref={ref => {
+                            if (ref && video.stream) {
+                                ref.srcObject = video.stream;
+                            }
+                        }}
+                        autoPlay
+                    />
+                ))}
+            </div>
+        </>
+    ) : (
+        videos.map((video) => (
             <video
+                key={video.socketId}
                 data-socket={video.socketId}
-                data-screen-share={video.isScreenShare ? "true" : undefined}
                 ref={ref => {
                     if (ref && video.stream) {
                         ref.srcObject = video.stream;
                     }
                 }}
                 autoPlay
-            >
-            </video>
-        </div>
-    ))}
+            />
+        ))
+    )}
 </div>
 
                 </div>
